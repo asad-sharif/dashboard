086fb365ae77f220066be3ab0c3d3dba53ab2af3
asad-sharif <asadsharif1270@gmail.com>
2025-09-09 23:38:04 +0500

    stats

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
+import AppAreaChart from '@/components/AppAreaChart'
 import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const Home = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-      <div className='bg-primary-foreground rounded-md p-2 col-span-2'>1</div>
+      <div className='bg-primary-foreground rounded-md py-4 pl-4 col-span-3'>
+        <h1 className='mb-2 font-bold text-2xl'>Overview</h1>
+        <AppAreaChart />
+      </div>
       <div className='bg-primary-foreground rounded-md p-2'>2</div>
       <div className='bg-primary-foreground rounded-md p-2'>3</div>
       <div className='bg-primary-foreground rounded-md p-2'>4</div>
       <div className='bg-primary-foreground rounded-md p-2 col-span-2'>5</div>
-      <div className='bg-primary-foreground rounded-md p-2'>6</div>
+      {/* <div className='bg-primary-foreground rounded-md p-2'>6</div> */}
     </div>
   )
 }
