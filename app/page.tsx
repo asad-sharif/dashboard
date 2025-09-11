4428fa08db21d870ed72e48f09ea4d42a5cdd09b
asad-sharif <asadsharif1270@gmail.com>
2025-09-11 23:38:08 +0500

    dashboard home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AppAreaChart from '@/components/AppAreaChart'
 import AppLineChart from '@/components/AppLineChart'
 import AppPieChart from '@/components/AppPieChart'
 import Navbar from '@/components/Navbar'
+import TodoList from '@/components/TodoList'
 import React from 'react'
 
 const Home = () => {
@@ -11,21 +12,24 @@ const Home = () => {
         <h1 className='mb-4 font-bold text-2xl'>Revenue Analytics</h1>
         <AppAreaChart />
       </div>
+
       <div className='bg-primary-foreground rounded-md py-4 pl-4 '>
         <h2 className='mb-2 font-semibold text-xl'>User Distribution by Browser</h2>
         <AppPieChart />
       </div>
-      <div className='bg-primary-foreground rounded-md p-4 flex flex-col items-start justify-center'>
+
+      {/* <div className='bg-primary-foreground rounded-md p-4 flex flex-col items-start justify-center'>
         <h2 className='mb-2 font-semibold text-lg'>Active Users</h2>
         <span className='text-3xl font-bold'>1,245</span>
         <span className='text-xs text-muted-foreground mt-1'>+5% this week</span>
+      </div> */}
+
+      <div className='bg-primary-foreground rounded-md p-4 '>
+        <h2 className='mb-2 font-semibold text-xl'>Todos</h2>
+        <TodoList />        
       </div>
-      <div className='bg-primary-foreground rounded-md p-4 flex flex-col items-start justify-center'>
-        <h2 className='mb-2 font-semibold text-lg'>Pending Tasks</h2>
-        <span className='text-3xl font-bold'>32</span>
-        <span className='text-xs text-muted-foreground mt-1'>Updated daily</span>
-      </div>
-      <div className='bg-primary-foreground rounded-md py-4 pl-4 col-span-2'>
+
+      <div className='bg-primary-foreground rounded-md py-4 pl-4 col-span-3'>
         <h2 className='mb-2 font-semibold text-xl'>Monthly Device Usage Trends</h2>
         <AppLineChart />
       </div>
---
+++ components/TodoList.tsx
@@ -0,0 +1,66 @@
+'use client'
+import React, { useState } from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Label } from "@/components/ui/label"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Button } from './ui/button'
+import { format } from 'date-fns'
+import { CalendarIcon } from 'lucide-react'
+import { Calendar } from "@/components/ui/calendar"
+
+const tasks = Array.from({ length: 10 }, (_, i) => ({
+  id: `task${i + 1}`,
+  text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+}));
+
+const TodoList = () => {
+  const [date, setDate] = useState<Date | undefined>(new Date())
+
+  return (
+    <div>
+      <Popover>
+        <PopoverTrigger asChild className='w-full my-2'>
+          <Button>
+            <CalendarIcon />
+            {date ? format(date, 'PP') : 'Pick a date'}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className='p-0 border-0 w-auto'>
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            className="rounded-md border"
+          />
+        </PopoverContent>
+      </Popover>
+
+      <ScrollArea className="h-[250px] px-2">
+        {tasks.map((task, index) => (
+          <Card key={index} className='p-0 my-2 rounded-sm'>
+            <CardContent className='flex items-center gap-2 p-2'>
+              <Checkbox id={task.id} checked={task.id === 'task1' || task.id === 'task4'} />
+              <Label htmlFor={task.id} className='text-xs text-muted-foreground'>{task.text}</Label>
+            </CardContent>
+          </Card>
+        ))
+        }
+      </ScrollArea>
+    </div >
+  )
+}
+
+export default TodoList
